6dac220677a6f39a40390acfa8960f37d5e13a26
bruno montebello <brunormontebello@hotmail.com>
2020-04-13 13:29:40 -0300

    Create Tea model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'teas/index'
+  get 'teas/show'
+  get 'teas/new'
+  get 'teas/create'
+  get 'teas/edit'
+  get 'teas/update'
+  get 'teas/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
