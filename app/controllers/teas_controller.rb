2afa834170cef4270a3f656d3e137cebf3251120
bruno montebello <brunormontebello@hotmail.com>
2020-04-13 14:50:34 -0300

    Create Tea model and controller

--- app/controllers/teas_controller.rb
+++ app/controllers/teas_controller.rb
@@ -1,22 +1,42 @@
 class TeasController < ApplicationController
   def index
+    @teas = Tea.all
   end
 
   def show
+    @tea = Tea.find(params[:id])
   end
 
   def new
+    @tea = Tea.new
   end
 
   def create
+    @tea = Tea.new(tea_params)
+    if @tea.save
+      redirect_to tea_path(@tea)
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @tea = Tea.find(params[:id])
   end
 
   def update
+    @tea = Tea.find(params[:id])
+    if @tea.update(tea_params)
+      redirect_to tea_path(@tea)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @tea = Tea.fidn(params[:id])
+    if @tea.destroy
+      redirect_to teas_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  get 'teas/index'
-  get 'teas/show'
-  get 'teas/new'
-  get 'teas/create'
-  get 'teas/edit'
-  get 'teas/update'
-  get 'teas/destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :teas, only: [:index, :show, :new, :edit]
 end
